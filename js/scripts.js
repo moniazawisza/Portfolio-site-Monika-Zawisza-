5a2db89c17ddcb401dea0e51cf5567dbe83859cb
moniazawisza <monika.zawisza1990@gmail.com>
2020-09-18 20:22:08 +0200

    add collapsible menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hi there! I'm so glad you're here!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === "navtoggle") {
+    x.className += "responsive";
+  } else {
+    x.className = 'navtoggle';
+  }
+}
